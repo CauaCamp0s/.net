a38430ae1cf892c0b917909f49298aacbd87d4b7
CauaCamp0s <cauacampos258@gmail.com>
2025-10-16 16:48:19 -0300

    Fix ajustes finais

--- BibliotecaAPI/obj/Debug/net8.0/BibliotecaAPI.AssemblyInfo.cs
+++ BibliotecaAPI/obj/Debug/net8.0/BibliotecaAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BibliotecaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cb6800eb7ca7d0b14d1877e3c0c41aa3ce3d983")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f26afe8ddc73a8ccbd78cf7a472e0786eda8304")]
 [assembly: System.Reflection.AssemblyProductAttribute("BibliotecaAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BibliotecaAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
